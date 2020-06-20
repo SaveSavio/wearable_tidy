c46fbcf24a0347a76db85486068d92a90ca92d0f
Joshua C. Parsell <joshua_parsell@hotmail.com>
2020-06-20 09:04:21 -0400

    Took out the function component so it is just a script does the actions as soon as you run it.

--- get_proj_data.R
+++ get_proj_data.R
@@ -1,7 +1,6 @@
-get_proj_data <- function () {
+
  
-    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","projdata.zip")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","projdata.zip")
     
-    unzip("projdata.zip")
+unzip("projdata.zip")
     
-}
